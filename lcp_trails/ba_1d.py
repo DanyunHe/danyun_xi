9b61bd2de28cf4411a200a3d2c1b108039eeb5a5
Xi Chen <chenx@g.harvard.edu>
2019-04-03 21:03:38 -0400

    1d

---
+++ lcp_trails/ba_1d.py
@@ -0,0 +1,238 @@
+import math
+from pygame import Rect
+import pygame
+import numpy as np
+from numpy import arctan as arctan
+from numpy import cos as cos
+from numpy import sin as sin
+
+  
+ 
+class Ball(object):
+
+    def _set_speed(self, speed_x, speed_y = 0):
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+
+    def _set_position(self,x,y):
+        self.x=x
+        self.y=y
+    
+    def __init__(self, x, speed_x, r, color, name, verbose, y = 50, speed_y = 0):
+        self.name = name
+        self.x = x
+        self.y = y
+        self._set_speed(speed_x,speed_y)
+        self.r = r
+        self.color = color
+        self.mass=r**3
+        self.verbose = verbose
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_speed_x(self):
+        return self.speed_x
+
+    def get_speed_y(self):
+        return self.speed_y
+
+    def get_new_x(self,dt):
+        return self.x+self.speed_x*dt
+
+    def get_new_y(self,dt):
+        return self.y+self.speed_y*dt
+
+    def get_params(self):
+        return [self.x,self.y,self.speed_x,self.speed_y]
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.r)
+
+
+    def get_energy(self):
+        return 0.5*self.mass*(self.speed_x**2+self.speed_y**2)
+     
+    def get_ball_col_time(self, o):
+        dist = np.abs(self.x - o.x)
+        v1x = self.speed_x
+        v2x = o.speed_x 
+
+        t_cand = np.abs(dist-self.r-o.r)/np.abs(v1x-v2x)
+
+        newx=self.get_new_x(t_cand)
+        onewx=o.get_new_x(t_cand)
+
+        if dist<np.abs(newx-onewx):
+            return 1e9
+        else:
+            return t_cand
+
+    def get_wall_col_time(self, box):
+        vx = self.speed_x
+        x = self.x
+
+        tmx, tpx = [np.abs((box.x - x)/vx) -np.abs(self.r/vx), np.abs((box.x + box.width - x)/vx)-np.abs(self.r/vx)]
+
+        if vx>0:
+            return tpx, 1
+        else:
+            return tmx, 0
+
+
+        #         np.array([np.abs((box.x-x)/vx)-np.abs(self.r/vx), np.abs((box.x + box.width-x)/vx)-np.abs(self.r/vx), 
+        #             np.abs((box.y-y)/vy)-np.abs(self.r/vy), np.abs((box.y + box.height-y)/vy)-np.abs(self.r/vy)])
+        # min_choice = np.argmin(col_times)
+
+        # return col_times[min_choice], min_choice
+
+    def move(self,dt):
+        self.x=self.get_new_x(dt)
+
+    def move_ball(self, o, dt):
+        self.x=self.get_new_x(dt)
+        o.x=o.get_new_x(dt)
+        mass=self.mass
+        o_mass=o.mass
+        m1 = mass
+        m2 = o_mass
+        if self.verbose:
+            print('old speed',self.speed_x,self.speed_y)
+
+        v1x = self.speed_x
+        v2x = o.speed_x
+
+        v1x, v2x = ((m1-m2)/(m1+m2)*v1x+(2*m2)/(m1+m2)*v2x,
+            (2*m1)/(m1+m2)*v1x+(m2-m1)/(m1+m2)*v2x)
+
+        # v1n, v2n = v2n, v1n
+        self.speed_x = v1x
+
+        o.speed_x = v2x    
+
+    def move_wall(self, dt, wall_index):
+        self.x=self.get_new_x(dt)
+        self.speed_x = -self.speed_x
+
+
+
+
+    # detect collision and calculate new speed and position, change speed if collision happens 
+    # tiemStep: dt  
+    def collision_with_other_ball(self, o,dt):
+
+        # if (self.x-o.x)**2+(self.y-o.y)**2>(self.r+o.r)**2:
+        #     self.status = 'overlap'
+
+        new_x=self.get_new_x(dt)
+        new_y=self.get_new_y(dt)
+        o_new_x=o.get_new_x(dt)
+        o_new_y=o.get_new_y(dt)
+
+
+        ## if the new position is overlapping and the current position not, or both are overlapping 
+        if ((new_x-o_new_x)**2+(new_y-o_new_y)**2<=(self.r+o.r)**2 and (self.x-o.x)**2+(self.y-o.y)**2>(self.r+o.r)**2) or \
+        ((new_x-o_new_x)**2+(new_y-o_new_y)**2<=(self.r+o.r)**2 and (self.x-o.x)**2+(self.y-o.y)**2<=(self.r+o.r)**2 and \
+            (new_x-o_new_x)**2+(new_y-o_new_y)**2<(self.x-o.x)**2+(self.y-o.y)**2):
+
+        #if (new_x-o_new_x)**2+(new_y-o_new_y)**2<=(self.r+o.r)**2:
+            if self.verbose:
+                print("collision happens")
+            mass=self.mass
+            o_mass=o.mass
+            m1 = mass
+            m2 = o_mass
+            if self.verbose:
+                print('old speed',self.speed_x,self.speed_y)
+
+            normal_tan = -(self.x - o.x)/(self.y - o.y)
+            normal_angle = arctan(normal_tan)
+            theta = normal_angle
+
+            v1x = self.speed_x
+            v1y = self.speed_y
+            v2x = o.speed_x
+            v2y = o.speed_y
+
+            v1t = v1x*cos(theta) + v1y*sin(theta)
+            v1n = v1x*sin(theta) - v1y*cos(theta)
+            v2t = v2x*cos(theta) + v2y*sin(theta)
+            v2n = v2x*sin(theta) - v2y*cos(theta)
+
+            v1n, v2n = ((m1-m2)/(m1+m2)*v1n+(2*m2)/(m1+m2)*v2n,
+                (2*m1)/(m1+m2)*v1n+(m2-m1)/(m1+m2)*v2n)
+
+            # v1n, v2n = v2n, v1n
+            self.speed_x = v1t*cos(theta) + v1n*sin(theta)
+            self.speed_y = v1t*sin(theta) - v1n*cos(theta)
+
+            o.speed_x = v2t*cos(theta) + v2n*sin(theta)
+            o.speed_y = v2t*sin(theta) - v2n*cos(theta)
+
+            # self.speed_x = (2*o_mass*o.speed_x+(mass-o_mass)*self.speed_x)/(mass+o_mass)
+            # self.speed_y = (2*o_mass*o.speed_y+(mass-o_mass)*self.speed_y)/(mass+o_mass)
+            # print('new speed p, q', self.speed_x, self.speed_y)
+            # o.speed_x = (2*mass*self.speed_x+(o_mass-mass)*o.speed_x)/(mass+o_mass)
+            # o.speed_y= (2*mass*self.speed_y+(o_mass-mass)*o.speed_y)/(mass+o_mass)
+  
+
+     # detect collition with wall, change speed if collision happens   
+    def collision_with_box(self, box, dt):
+        #check vertical collision:
+        self.detect_collision_with_vertical_line(box.x, dt)
+        self.detect_collision_with_vertical_line(box.x + box.width, dt)
+        self.detect_collision_with_horizontal_line(box.y, dt)
+        self.detect_collision_with_horizontal_line(box.y + box.height, dt)
+       
+    # detect collition with vertical line, change speed if collision happens 
+    # x: vertical line position 
+    # dt: move by time step dt 
+    def collision_with_vertical_line(self, x,dt):
+        new_x=self.get_new_x(dt)
+        if abs(new_x-x)<=self.r:
+            if self.verbose:
+                print('collision with vertical line')
+            self.speed_x=-self.speed_x
+
+            
+    def collision_with_horizontal_line(self, y, dt):
+        new_y=self.get_new_y(dt)
+        if abs(new_y-y)<=self.r:
+            if self.verbose:
+                print('collision with horizontal line')
+            self.speed_y=-self.speed_y
+
+        
+    
+    def log(self, description):
+        if self.verbose:
+            print(description, self.name, self.x, self.y, self.speed_x, self.speed_y)
+    
+   
+# COLLISION TESTING
+if __name__=="__main__":
+    #ball goes against the wall
+    b3 = Ball(20, 40, 3, 0, 5, None)
+    box = Rect(0, 0, 400, 400)
+    b3.detect_collision_with_box(box,0.001)
+    #print 'collision time', b3.collision_response.t
+    #print 'b3 new speed', b3.collision_response.new_speed_x, b3.collision_response.new_speed_y
+   
+    #print 'b3 x, y', b3.x, b3.y
+    b1 = Ball(300, 30, 5, -30, 15, None)
+    box2 = Rect(5, 5, 490, 590)
+    print('before collision', b1.speed_x, b1.speed_y)
+    b1.detect_collision_with_box(box2, 1000)
+   
+    print('b1 after update',  b1.x, b1.y, b1.speed_x, b1.speed_y)
+    b1.detect_collision_with_box(box2, 2)
+  
+    b1.log('before moving')
+
+    b1.move(1)
+    b1.log('after moving')
+        
+    
---
+++ lcp_trails/ball_world_1d.py
@@ -0,0 +1,162 @@
+from ba_1d import Ball
+import pygame
+from pygame import Color, Rect
+import sys
+from math import fabs
+import numpy as np
+import math
+import random
+
+class BallWorld(object):
+	SCREEN_WIDTH, SCREEN_HEIGHT = 600, 100
+	def __init__(self,x1=30,y1=30,x2=70,y2=70,x3=150,y3=50,x4=150, y4=150,\
+		speed_x1=1000,speed_y1=0,speed_x2=-1000,speed_y2=0,speed_x3=300,speed_y3=-1000,speed_x4=1000, speed_y4=1000,\
+		left=0, top=0, width=100, height=100, verbose = 0):
+		pygame.init()
+		self.verbose = verbose
+		self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 0, 32)
+		self.clock = pygame.time.Clock()
+		self.balls = []
+		# initialize Ball(x,y,speed_x,speed_y,r,color,name)
+		self.balls.append(Ball(x1, speed_x1, 10, Color('yellow'), 'yellow', self.verbose))
+		self.balls.append(Ball(x2, speed_x2, 20, Color('red'),'red', self.verbose))
+		self.balls.append(Ball(x3, speed_x3, 30, Color('blue'), 'blue', self.verbose))	
+		#self.balls.append(Ball(x4, speed_x4, 10, Color('green'), 'green', self.verbose))	
+		# initialize wall 	
+		self.border = Rect(left, top, width, height)
+		self.border.left=left
+		self.border.top=top
+		self.border.width=width
+		self.border.height=height
+	
+
+	def update(self,dt):
+		t_remaining = dt
+
+		ball_hit_ball = np.array([0,0])
+		ball_hit_wall = np.array([0])
+
+		while True:
+			ball_col_time_min = dt
+			wall_col_time_min = dt
+			for i in range(len(self.balls)):
+				for j in range(i+1,len(self.balls)):
+					ball_col_time = self.balls[i].get_ball_col_time(self.balls[j])
+					if ball_col_time < ball_col_time_min and ball_col_time>0:
+						ball_col_time_min = ball_col_time 
+						ball_hit_ball[0] = i
+						ball_hit_ball[1] = j
+				wall_col_time, index = self.balls[i].get_wall_col_time(self.border) # the wall collision time is the minimum of 4
+				if wall_col_time < wall_col_time_min and wall_col_time>0:
+					wall_col_time_min = wall_col_time 
+					ball_hit_wall = i
+					wall_index = index
+
+			if np.min((wall_col_time_min, ball_col_time_min)) < t_remaining:
+
+				## if there is still time for events
+				if wall_col_time_min < ball_col_time_min:
+					self.balls[ball_hit_wall].move_wall(wall_col_time_min, wall_index)
+					for i in np.setdiff1d(np.arange(len(self.balls)),np.array([ball_hit_wall])):
+						self.balls[i].move(wall_col_time_min)
+					t_remaining -= wall_col_time_min
+							 # update is done by calling move
+
+				else:
+					self.balls[ball_hit_ball[0]].move_ball(self.balls[ball_hit_ball[1]], ball_col_time_min)
+					for i in np.setdiff1d(np.arange(len(self.balls)),np.array([ball_hit_ball])):
+						self.balls[i].move(ball_col_time_min)
+					t_remaining -= ball_col_time_min
+
+
+			else:
+
+				## no more collision is going to happen. Just normally move 't_remaining'
+				for i in range(len(self.balls)):
+					self.balls[i].move(t_remaining)
+				break
+
+
+
+
+			## keep a track of what is happening within dt
+			## possible outcome:(1) hit the wall (2) hit one other ball 
+			## if hit one wall before hitting anything else
+
+
+		# #check collision with other balls
+		# for i in range(len(self.balls)):
+		# 	for j in range(len(self.balls)):
+		# 		if i < j:
+		# 			self.balls[i].collision_with_other_ball(self.balls[j],dt)
+
+		# #check collision with box border:
+		# for b in self.balls:
+		# 	b.collision_with_box(self.border,dt)
+		
+		# # move by time step dt
+		# ene = 0
+		# for b in self.balls:
+		# 	b.log('ball ')
+		# 	b.move(dt)
+		# 	ene += b.get_energy()
+		# print("Total energy:", ene)
+			
+
+	def log(self, ball, description):
+		if self.verbose:
+			print(description, 'x', ball.x, 'y', ball.y)
+
+	def draw(self):
+		pygame.draw.rect(self.screen, Color("grey"), self.border)
+		for b in self.balls:
+			b.draw(self.screen)
+	
+	def quit(self):
+		sys.exit()
+
+	# N: number of episodes
+	# T: number of steps
+	# dt: size of time step 
+	def run(self,N,T,dt, verbose = 0):
+		pygame.key.set_repeat(30, 30)
+		params=[] # data [t,num_balls,4], contain position and velocity information at all the time for all the balls
+		for _ in range(N):
+
+			# reset the position and velocity of balls 
+			# for i in range(2):
+			# 	angle=random.uniform(0,2*math.pi)
+			# 	speed_x=10*math.cos(angle)
+			# 	speed_y=10*math.sin(angle)
+			# 	self.balls[i]._set_speed(speed_x,speed_y)
+			# 	x=random.uniform(self.border.left,self.border.width-self.border.left)
+			# 	y=random.uniform(self.border.top,self.border.height-self.border.top)
+			# 	self.balls[i]._set_position(x,y)
+
+			for _ in range(T):
+				self.update(dt)
+				for b in self.balls:
+					params.append(b.get_params())
+
+				self.draw()
+				for e in pygame.event.get():
+					#if e.type == pygame.KEYDOWN:
+						#if e.key == pygame.K_RETURN:
+							#self.update()
+							#self.draw()
+					if e.type == pygame.QUIT:
+						self.quit()		
+				pygame.display.flip()								
+		return params
+
+if __name__=="__main__":
+	random.seed(77)
+	# initial balls position, speed, and box size 
+	bw = BallWorld(x1=30.1,y1=30.2,x2=70.1,y2=70.2,x3=250.1,y3=100.2, x4=150, y4=150,\
+		speed_x1=500,speed_y1=300,speed_x2=-500,speed_y2=1,speed_x3=300,speed_y3=-1000, speed_x4=1000, speed_y4=1000,\
+		left=0, top=0, width=600, height=100, verbose = 0)
+
+	params=bw.run(1,20000,0.01) # generate data 
+	if verbose:
+		print(params[0])
+		print(np.array(params).shape)
